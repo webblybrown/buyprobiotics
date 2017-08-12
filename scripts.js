53aa90d87d652294246cb7ec7b05b46edf4555b9
webblybrown <info@wesleybrown.co.uk>
2017-08-12 22:02:33 +0100

    Initial

---
+++ scripts.js
@@ -0,0 +1,18 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function($) {
+  console.log('loaded');
+})(jQuery);
+
+WebFontConfig = {
+  google: {
+    families: ['Lato:400, 700']
+  },
+  timeout: 2000
+};
+(function(d) {
+  var wf = d.createElement('script'), s = d.scripts[0];
+  wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.16/webfont.js';
+  s.parentNode.insertBefore(wf, s);
+})(document);
+
+},{}]},{},[1])
